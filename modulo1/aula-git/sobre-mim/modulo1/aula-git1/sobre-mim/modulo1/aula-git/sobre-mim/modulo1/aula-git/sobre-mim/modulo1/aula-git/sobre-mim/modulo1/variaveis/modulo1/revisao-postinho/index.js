ed1fd381a203d753767ef022b42132cc674642e5
Evellem Dhane dos Santos Oliveira <eves2017@hotmail.com>
2022-05-23 11:51:33 -0300

    exercicio-reforco-js-resolucao-ate-a-4

--- modulo1/aula-git/sobre-mim/modulo1/aula-git1/sobre-mim/modulo1/aula-git/sobre-mim/modulo1/aula-git/sobre-mim/modulo1/aula-git/sobre-mim/modulo1/variaveis/modulo1/revisao-postinho/index.js
+++ modulo1/aula-git/sobre-mim/modulo1/aula-git1/sobre-mim/modulo1/aula-git/sobre-mim/modulo1/aula-git/sobre-mim/modulo1/aula-git/sobre-mim/modulo1/variaveis/modulo1/revisao-postinho/index.js
@@ -30,7 +30,7 @@ console.log(verificaSeEMaior(321, 2156));
 //Escreva true or false para as comparações abaixo:
 // exemplo 1>2 = false
 // a-) 1==='1'= false
-// b-) 1=='1'= 
+// b-) 1=='1'= false
 // c-) 'a'==='b'= false
 // d-) 'b'>'a'= false
 // e-) 0!==null= true
@@ -41,17 +41,34 @@ console.log(verificaSeEMaior(321, 2156));
 // Exercício 3------------------------------------------------------------------------------------
 
 const cadastro = () => {
-    const usuario = []
+    const nomeDoUsuario = prompt("Qual seu nome?")
+    const anoDeNascimento = prompt ("Qual seu ano de nascimento?")
+    if (anoDeNascimento <= "2004"){
+        console.log("Somente maiores de 18 anos podem realizar o cadastro")
+    }
 
-    //  Sua lógica aqui
-
+    const nacionalidade = prompt ("Qual a sua nacionalidade?")
+    if (nacionalidade === "brasileiro"){
+        console.log("Somente brasileiros podem realizar o cadastro")
+    }
+    const senhaDoUsuario = prompt ("Digite sua senha aqui")
+    if (senhaDoUsuario == "123456") {
+        console.log("Cadastro concluído")
+    }
 }
+    //  Sua lógica aqui
 console.log(cadastro());
 
 // Exercício 4-----------------------------------------------------------------------------------------------
 
 const login = () => {
     const login = "labenu"
+    const senha = prompt("Digite aqui sua senha de acesso")
+    if(senha === "labenu"){
+        console.log("Usuário logado")
+    }
+    else if( senha !== "labenu")
+    console.log("Senha inválida")
     //  Sua lógica aqui
 
 }
@@ -61,6 +78,11 @@ console.log(login());
 // Exercício 5----------------------------------------------------------------------------------------------------
 
 const primeiraDose = () => {
+const nome = prompt("Qual seu nome?")
+const vacina = prompt ("Qual vacina você tomou?")
+
+
+alert (`Olá ${nome}! A próxima dose da ${vacina} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`)
 
     //  Sua lógica aqui
 
